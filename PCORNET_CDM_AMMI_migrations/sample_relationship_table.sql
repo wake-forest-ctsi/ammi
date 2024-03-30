ef6ef9ecc4e03bc6424068bb28af82e84bae9326
KobbySekyere <71403624+KobbySekyere@users.noreply.github.com>
2024-03-30 13:21:04 -0400

    Create sample_relationship_table.sql
    SQL to populate relationship_table

---
+++ PCORNET_CDM_AMMI_migrations/sample_relationship_table.sql
@@ -0,0 +1,23 @@
+Insert into AMMI.[dbo].BIRTH_RELATIONSHIP( [BIRTHID]
+      ,[PREGNANCYID]
+      ,[PATID]
+      ,[ENCOUNTERID]
+      ,[MOTHERID]
+      ,[MOTHER_ENCOUNTERID])
+
+ SELECT distinct
+     wk.SUMMARY_BLOCK_ID as birthid
+		,wk.PREG_EPISODE_ID as pregnancyid 
+		,wk.BABY_PATIENT_NUM as pat_id
+		,wk.BABY_ENCOUNTER_NUM as encounterid
+		,wk.MOM_PATIENT_NUM as motherid
+	  ,wk.[MOM_ENCOUNTER_NUM] as mother_encounterid	  
+	
+	 
+FROM [AMMI].[dbo].[ammi_preg_pcornet__3_28_2024] wk 
+
+join [AMMI].[dbo].DEMOGRAPHIC d2 on d2.patid = wk.baby_PATIENT_NUM
+join [AMMI].[dbo].DEMOGRAPHIC d on d.patid = wk.MOM_PATIENT_NUM
+join [AMMI].[dbo].ENCOUNTER e2 on e2.ENCOUNTERID = wk.BABY_ENCOUNTER_NUM 
+join [AMMI].[dbo].ENCOUNTER e on e.ENCOUNTERID = wk.MOM_ENCOUNTER_NUM                             
+where wk.BABY_PATIENT_NUM is not null 
