eeea171dd4ee5c17d11ccdffcdf2727e85a85302
Michael Horvath <mhorvath@wakehealth.edu>
2024-04-23 15:41:04 -0400

    fix preeclampsia additional features

--- pcornet_analysis.sql
+++ pcornet_analysis.sql
@@ -6,19 +6,19 @@ with pregnancy_episodes as
 		dateadd(MONTH,-9,encounter.admit_date) as episode_begin_datetime,
 		encounter.admit_date as episode_end_datetime
 	from
-		BIRTH_RELATIONSHIP
-		join encounter on encounter.ENCOUNTERID = birth_relationship.MOTHER_ENCOUNTERID
+		birth_relationship
+		join encounter on encounter.encounterid = birth_relationship.mother_encounterid
 ),
 pregnancy_vitals as
 (
 	select
 		pregnancy_episodes.*,
-		vital.SYSTOLIC,
-		vital.DIASTOLIC,
-		vital.MEASURE_DATE + vital.MEASURE_TIME as measure_datetime
+		vital.systolic,
+		vital.diastolic,
+		vital.measure_date + vital.measure_time as measure_datetime
 	from vital 
 	join pregnancy_episodes on 
-		vital.PATID = pregnancy_episodes.patid
+		vital.patid = pregnancy_episodes.patid
 		and (vital.measure_date + vital.measure_time) between episode_begin_datetime and episode_end_datetime
 ),
 hdp_vitals as
@@ -35,7 +35,7 @@ hdp_vitals as
 		b.diastolic as diastolic_b,
 		b.measure_datetime as measure_datetime_b,
 		case when a.systolic >= 160 and b.systolic >= 160 and a.diastolic >= 110 and b.diastolic >= 110 then 1 else 0 end severe_hypertension,
-		(a.systolic - 140) + (b.systolic - 140) + (a.diastolic - 90) + (b.diastolic - 90) as hypertension_score  --
+		(a.systolic - 140) + (b.systolic - 140) + (a.diastolic - 90) + (b.diastolic - 90) as hypertension_score
 	from
 		pregnancy_vitals a
 		join pregnancy_vitals b on
@@ -45,14 +45,6 @@ hdp_vitals as
 	where
 		a.systolic >= 140
 		or a.diastolic >= 90
-),
-hdp_vitals_rank as
-(
-	select
-		row_number() over (partition by hdp_vitals.birthid order by hdp_vitals.severe_hypertension desc, hdp_vitals.hypertension_score desc) as rownum,
-		hdp_vitals.*
-	from
-		hdp_vitals
 ),
 hdp_vitals_max as
 (
@@ -66,10 +58,14 @@ hdp_vitals_max as
 		hdp_vitals_rank.measure_datetime_a,
 		hdp_vitals_rank.systolic_b,
 		hdp_vitals_rank.diastolic_b,
-		hdp_vitals_rank.measure_datetime_b,
-		hdp_vitals_rank.severe_hypertension
+		hdp_vitals_rank.measure_datetime_b
+		--,hdp_vitals_rank.severe_hypertension
 	from
-		hdp_vitals_rank
+		(select
+			row_number() over (partition by hdp_vitals.birthid order by hdp_vitals.severe_hypertension desc, hdp_vitals.hypertension_score desc) as rownum,
+			hdp_vitals.*
+		from
+			hdp_vitals) hdp_vitals_rank
 	where
 		hdp_vitals_rank.rownum = 1
 ),
@@ -80,7 +76,12 @@ hdp_labs as
 		lab_result_cm.lab_loinc,
 		lab_result_cm.result_num,
 		lab_result_cm.result_modifier,
-		lab_result_cm.specimen_date + lab_result_cm.specimen_time as specimen_datetime
+		lab_result_cm.specimen_date + lab_result_cm.specimen_time as specimen_datetime,
+		case when lab_result_cm.lab_loinc in ('2889-4') then 1
+			when lab_result_cm.lab_loinc in ('2890-2') then 2
+			when lab_result_cm.lab_loinc in ('777-3') then 3
+			when lab_result_cm.lab_loinc in ('2160-0') then 4
+		end as preeclampsia_features
 	from lab_result_cm
 	join pregnancy_episodes on 
 		lab_result_cm.PATID = pregnancy_episodes.patid
@@ -97,11 +98,34 @@ hdp_labs as
 				-- Pulmonary Edema?
 				-- New onset headache?
 			)
+),
+hdp_labs_max as (
+		select
+		hdp_labs_rank.patid,
+		hdp_labs_rank.birthid,
+		hdp_labs_rank.episode_begin_datetime,
+		hdp_labs_rank.episode_end_datetime,
+		hdp_labs_rank.lab_loinc,
+		hdp_labs_rank.result_num,
+		hdp_labs_rank.result_modifier,
+		hdp_labs_rank.specimen_datetime
+	from
+		(select
+			row_number() over (partition by hdp_labs.birthid order by hdp_labs.preeclampsia_features asc) as rownum,
+			hdp_labs.*
+		from
+			hdp_labs) hdp_labs_rank
+	where
+		hdp_labs_rank.rownum = 1
+	
 )
 select
-	hdp_vitals_max.*
-	, hdp_labs.*
+	hdp_vitals_max.*,
+	hdp_labs_max.lab_loinc,
+	hdp_labs_max.result_num,
+	hdp_labs_max.result_modifier,
+	hdp_labs_max.specimen_datetime
 from
 	hdp_vitals_max
-	inner join hdp_labs on hdp_labs.birthid = hdp_vitals_max.birthid
+	inner join hdp_labs_max on hdp_labs_max.birthid = hdp_vitals_max.birthid
 ;
