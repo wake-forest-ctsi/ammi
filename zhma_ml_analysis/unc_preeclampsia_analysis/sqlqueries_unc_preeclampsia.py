bf7cd8be260695519a4bd42ed062bcd81dcbfc2c
Zhitu Ma <zhma@wakehealth.edu>
2024-11-06 12:15:38 -0500

    remove old queries form .py file

--- zhma_ml_analysis/unc_preeclampsia_analysis/sqlqueries_unc_preeclampsia.py
+++ zhma_ml_analysis/unc_preeclampsia_analysis/sqlqueries_unc_preeclampsia.py
@@ -61,56 +61,6 @@ FROM demographic_base a LEFT JOIN AMMI.dbo.DEMOGRAPHIC b ON a.MOTHERID = b.PATID
 ORDER BY a.BIRTHID
 '''
 
-blood_pressure_sql_string_old = demographic_base_sql_string + ',' + '''
-vital_tmp AS
-(
-SELECT
-  PATID,
-  CASE WHEN (SYSTOLIC < 140) AND (DIASTOLIC < 90) THEN 0 
-       WHEN (SYSTOLIC BETWEEN 140 AND 159) OR (DIASTOLIC BETWEEN 90 AND 109) THEN 1
-       WHEN (SYSTOLIC > 159) OR (DIASTOLIC > 109) THEN 2 END AS "bp_cat",
-  MEASURE_DATE + MEASURE_TIME AS "measure_date"
-FROM AMMI.dbo.VITAL
-WHERE SYSTOLIC IS NOT NULL AND DIASTOLIC IS NOT NULL
-)
-,
--- select the time period: <20wks, 20-delivery, during delivery, 90d after delivery
-vital_tmp2 AS
-(
-SELECT
-  a.BIRTHID,
-  b.bp_cat,
-  b.measure_date,
-  CASE WHEN b.measure_date < DATEADD(WEEK, 20, a.preg_start_date) THEN 0
-       WHEN b.measure_date BETWEEN DATEADD(WEEK, 20, a.preg_start_date) AND a.delivery_admission_date THEN 1
-	   WHEN b.measure_date BETWEEN a.delivery_admission_date AND a.delivery_discharge_date THEN 2
-	   WHEN b.measure_date BETWEEN a.delivery_discharge_date AND DATEADD(DAY, 90, a.delivery_discharge_date) THEN 3
-       ELSE NULL END AS "time_period"
-FROM demographic_base a INNER JOIN vital_tmp b ON a.MOTHERID = b.PATID
-)
-,
--- the 4h criteria, this includes bp_cat=0, which may not be correct?
-vital_tmp3 AS
-(
-SELECT
-  BIRTHID,
-  time_period,
-  bp_cat
-FROM vital_tmp2
-WHERE time_period IS NOT NULL
-GROUP BY BIRTHID, bp_cat, time_period
-HAVING DATEDIFF(HOUR, MIN(measure_date), MAX(measure_date)) >= 4
-)
-
-SELECT
-  BIRTHID,
-  time_period,
-  MAX(bp_cat) AS "bp_cat"
-FROM vital_tmp3
-GROUP BY BIRTHID, time_period
-ORDER BY 1,2,3;
-'''
-
 blood_pressure_sql_string = demographic_base_sql_string + '''
 SELECT
   a.BIRTHID,
@@ -196,60 +146,6 @@ SELECT DISTINCT
   PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY OBSCLIN_RESULT_NUM) OVER (PARTITION BY BIRTHID, RAW_OBSCLIN_NAME) AS 'median_VALUE'
 FROM obs_tmp
 ORDER BY BIRTHID;
-'''
-
-preeclampsia_sql_string_old = demographic_base_sql_string + ',' + '''
-preeclampsia_diagnosis AS
-(
-select 
-    a.BIRTHID,
-    min(a.preg_start_date) as 'preg_start_date',
-    min(b.dx_date) as 'earliest_diagnosis_date',
-    max(b.dx_date) as 'latest_diagnosis_date'
-from 
-    demographic_base a
-    left join 
-    (select dx_type, dx, dx_date, patid 
-    from AMMI.dbo.DIAGNOSIS
-    where dx_type = '10' and (dx like 'O14%' or dx like 'O15%')) b
-    on a.motherid = b.patid
-        and b.dx_date between a.preg_start_date AND a.preg_end_date
-group by a.BIRTHID
-)
-,
-/* Birth episodes with preeclampsia supporting laboratory measurements */
-lab_tmp as
-(
-select 
-    *
-from AMMI.dbo.LAB_RESULT_CM a
-where 
-   (a.lab_loinc in ('2889-4') and a.result_num >= 300 and a.result_modifier in ('EQ', 'GE', 'GT'))		-- Proteinuria
-or (a.lab_loinc in ('2890-2') and a.result_num >= 0.3 and a.result_modifier in ('EQ', 'GE', 'GT'))	-- Proteinuria
--- Proteinuria dipstick ??
-or (a.lab_loinc in ('777-3') and a.result_num <= 99999 and a.result_modifier in ('EQ', 'LE', 'LT'))	-- Thrombocytopenia
-or (a.lab_loinc in ('2160-0') and a.result_num >= 1.2 and a.result_modifier in ('EQ', 'GE', 'GT'))	-- Renal Insufficiency
-)
-,
-lab_diagnosis as
-(
-select
-    a.BIRTHID,
-    min(b.lab_order_date) as 'earliest_lab_confirm_date',
-    max(b.lab_order_date) as 'latest_lab_confirm_date'
-from demographic_base a
-left join lab_tmp b on a.motherid = b.patid and b.lab_order_date between a.preg_start_date and a.cutoff_time
-group by a.birthid
-)
-SELECT
-a.BIRTHID,
-a.earliest_diagnosis_date,
-a.latest_diagnosis_date,
-b.earliest_lab_confirm_date,
-b.latest_lab_confirm_date
-FROM preeclampsia_diagnosis a
-LEFT JOIN lab_diagnosis b ON a.BIRTHID = b.BIRTHID
-ORDER BY a.BIRTHID;
 '''
 
 preeclampsia_sql_string = demographic_base_sql_string + ',' + '''
