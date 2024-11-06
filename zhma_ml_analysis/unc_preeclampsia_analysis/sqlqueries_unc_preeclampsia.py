84ae9292115518b7b2cde493e3750a74b760e736
Zhitu Ma <zhma@wakehealth.edu>
2024-11-06 12:06:42 -0500

    updated preeclampsia definition and blood pressure feature; remove gestational_age in model

--- zhma_ml_analysis/unc_preeclampsia_analysis/sqlqueries_unc_preeclampsia.py
+++ zhma_ml_analysis/unc_preeclampsia_analysis/sqlqueries_unc_preeclampsia.py
@@ -49,6 +49,7 @@ SELECT
   MAX(CASE WHEN b.RACE = '03' THEN 1 ELSE 0 END) AS "is_black"
 FROM demographic_base a LEFT JOIN AMMI.dbo.DEMOGRAPHIC b ON a.MOTHERID = b.PATID
 GROUP BY a.BIRTHID
+ORDER BY a.BIRTHID
 '''
 
 age_sql_string = demographic_base_sql_string + '''
@@ -57,9 +58,10 @@ SELECT
   a.gest_age_in_days,
   DATEDIFF(YEAR, b.BIRTH_DATE, a.preg_start_date) AS "mom_age"
 FROM demographic_base a LEFT JOIN AMMI.dbo.DEMOGRAPHIC b ON a.MOTHERID = b.PATID
+ORDER BY a.BIRTHID
 '''
 
-blood_pressure_sql_string = demographic_base_sql_string + ',' + '''
+blood_pressure_sql_string_old = demographic_base_sql_string + ',' + '''
 vital_tmp AS
 (
 SELECT
@@ -81,8 +83,8 @@ SELECT
   b.measure_date,
   CASE WHEN b.measure_date < DATEADD(WEEK, 20, a.preg_start_date) THEN 0
        WHEN b.measure_date BETWEEN DATEADD(WEEK, 20, a.preg_start_date) AND a.delivery_admission_date THEN 1
-	     WHEN b.measure_date BETWEEN a.delivery_admission_date AND a.delivery_discharge_date THEN 2
-	     WHEN b.measure_date BETWEEN a.delivery_discharge_date AND DATEADD(DAY, 90, a.delivery_discharge_date) THEN 3
+	   WHEN b.measure_date BETWEEN a.delivery_admission_date AND a.delivery_discharge_date THEN 2
+	   WHEN b.measure_date BETWEEN a.delivery_discharge_date AND DATEADD(DAY, 90, a.delivery_discharge_date) THEN 3
        ELSE NULL END AS "time_period"
 FROM demographic_base a INNER JOIN vital_tmp b ON a.MOTHERID = b.PATID
 )
@@ -107,6 +109,26 @@ SELECT
 FROM vital_tmp3
 GROUP BY BIRTHID, time_period
 ORDER BY 1,2,3;
+'''
+
+blood_pressure_sql_string = demographic_base_sql_string + '''
+SELECT
+  a.BIRTHID,
+  AVG(b.SYSTOLIC) AS 'mean_sbp_value',
+  AVG(b.DIASTOLIC) AS 'mean_dbp_value',
+  MAX(b.SYSTOLIC) AS 'max_sbp_value',
+  MAX(b.DIASTOLIC) AS 'max_dbp_value',
+  MIN(b.SYSTOLIC) AS 'min_sbp_vale',
+  MIN(b.DIASTOLIC) AS 'min_dbp_value',
+  AVG(b.SYSTOLIC - b.DIASTOLIC) AS 'mean_pulse_pressure',
+  MAX(b.SYSTOLIC - b.DIASTOLIC) AS 'max_pulse_pressure',
+  MIN(b.SYSTOLIC - b.DIASTOLIC) AS 'min_pulse_pressure'
+FROM demographic_base a
+LEFT JOIN AMMI.dbo.VITAL b ON a.MOTHERID = b.PATID
+ AND b.measure_date < DATEADD(WEEK, 20, a.preg_start_date)
+ AND b.SYSTOLIC IS NOT NULL AND b.DIASTOLIC IS NOT NULL
+GROUP BY a.BIRTHID
+ORDER BY a.BIRTHID
 '''
 
 vital_sql_string = demographic_base_sql_string + ',' + '''
@@ -148,6 +170,7 @@ SELECT
   b.mean_HEIGHT
   -- b.median_HEIGHT
 FROM vital_1 a LEFT JOIN vital_2 b ON a.BIRTHID = b.BIRTHID
+ORDER BY a.BIRTHID
 '''
 
 obs_sql_string = demographic_base_sql_string + ',' + '''
@@ -175,7 +198,7 @@ FROM obs_tmp
 ORDER BY BIRTHID;
 '''
 
-preeclampsia_sql_string = demographic_base_sql_string + ',' + '''
+preeclampsia_sql_string_old = demographic_base_sql_string + ',' + '''
 preeclampsia_diagnosis AS
 (
 select 
@@ -229,6 +252,190 @@ LEFT JOIN lab_diagnosis b ON a.BIRTHID = b.BIRTHID
 ORDER BY a.BIRTHID;
 '''
 
+preeclampsia_sql_string = demographic_base_sql_string + ',' + '''
+vital_tmp AS
+(
+SELECT
+  a.BIRTHID,
+  MEASURE_DATE + MEASURE_TIME AS 'measure_date',
+  SYSTOLIC,
+  DIASTOLIC,
+  CASE WHEN (SYSTOLIC >= 160) OR (DIASTOLIC >= 110) THEN 2
+	   WHEN (SYSTOLIC >= 140) OR (DIASTOLIC >= 90) THEN 1	
+	   ELSE 0 END AS "bp_cat",
+  CASE WHEN MEASURE_DATE < DATEADD(WEEK, 20, a.preg_start_date) THEN 0
+       WHEN MEASURE_DATE BETWEEN DATEADD(WEEK, 20, a.preg_start_date) AND DATEADD(DAY, 90, a.preg_end_date) THEN 1 END AS "period"
+FROM demographic_base a
+INNER JOIN AMMI.dbo.VITAL b ON a.MOTHERID = b.PATID
+ AND (b.SYSTOLIC IS NOT NULL AND b.DIASTOLIC IS NOT NULL AND MEASURE_DATE IS NOT NULL)
+ AND b.MEASURE_DATE <= DATEADD(DAY, 90, a.preg_end_date)
+)
+,
+-- the 4h criteria
+vital_tmp_4h AS
+(
+SELECT
+  BIRTHID,
+  period,
+  bp_cat
+FROM vital_tmp
+WHERE bp_cat > 0
+GROUP BY BIRTHID, period, bp_cat
+HAVING DATEDIFF(HOUR, MIN(measure_date), MAX(measure_date)) >= 4 
+)
+-- get the max bp_cat for each patient and period
+,
+bp_cat_tmp AS
+(
+SELECT
+  BIRTHID,
+  period,
+  MAX(bp_cat) AS 'bp_cat'
+FROM vital_tmp_4h
+GROUP BY BIRTHID, period
+)
+,
+-- the bp cat variable
+bp_cat AS
+(
+SELECT
+  a.BIRTHID,
+  CASE WHEN b.bp_cat_before_20wk IS NULL THEN 0 ELSE b.bp_cat_before_20wk END AS 'bp_cat_before_20wk',
+  CASE WHEN c.bp_cat_after_20wk IS NULL THEN 0 ELSE c.bp_cat_after_20wk END AS 'bp_cat_after_20wk'
+FROM 
+(SELECT DISTINCT BIRTHID FROM demographic_base) a
+LEFT JOIN (SELECT BIRTHID, bp_cat AS 'bp_cat_before_20wk' FROM bp_cat_tmp WHERE period = 0) b 
+  ON a.BIRTHID = b.BIRTHID
+LEFT JOIN (SELECT BIRTHID, bp_cat AS 'bp_cat_after_20wk' FROM bp_cat_tmp WHERE period = 1) c
+  ON a.BIRTHID = c.BIRTHID
+)
+,
+-- chronic hypertension
+chtn AS 
+(
+SELECT
+  a.BIRTHID,
+  CASE WHEN MAX(b.bp_cat_before_20wk) > 0 THEN 1 
+       WHEN MAX(CASE WHEN DX_DATE IS NOT NULL THEN 1 ELSE 0 END) > 0 THEN 1 
+	   ELSE 0 END AS 'chtn_any'
+FROM demographic_base a
+LEFT JOIN bp_cat b ON a.BIRTHID = b.BIRTHID
+LEFT JOIN (SELECT * FROM AMMI.dbo.DIAGNOSIS WHERE DX LIKE 'I1%') c 
+  ON a.MOTHERID = c.PATID AND c.DX_DATE < DATEADD(WEEK, 20, a.preg_start_date)
+GROUP BY a.BIRTHID
+)
+,
+-- magnisium infusion
+mag_infuse AS
+(
+SELECT
+  a.BIRTHID,
+  MAX(CASE WHEN b.MEDADMIN_START_DATE IS NOT NULL THEN 1 ELSE 0 END) AS 'mag_infuse'
+FROM demographic_base a
+LEFT JOIN AMMI.dbo.MED_ADMIN b ON a.MOTHERID = b.PATID
+ AND b.RAW_MEDADMIN_MED_NAME LIKE '%MAGNESIUM SULFATE%'
+ AND b.MEDADMIN_START_DATE BETWEEN DATEADD(WEEK, 20, a.preg_start_date) AND DATEADD(DAY, 90, a.preg_end_date)
+GROUP BY a.BIRTHID
+)
+,
+-- pre-eclampsia with severe feature or eclampsia
+icd_10_pre_sf AS
+(
+SELECT
+  a.BIRTHID,
+  MAX(CASE WHEN b.DX_DATE IS NOT NULL THEN 1 ELSE 0 END) AS 'pre_sf_or_ecl'
+FROM demographic_base a
+LEFT JOIN AMMI.dbo.DIAGNOSIS b ON a.MOTHERID = b.PATID
+ AND (b.DX LIKE 'O14.1%' OR b.DX LIKE 'O14.2%' OR b.DX LIKE 'O15%')
+ AND b.DX_DATE BETWEEN DATEADD(WEEK, 20, a.preg_start_date) AND DATEADD(DAY, 90, a.preg_end_date)
+GROUP BY a.BIRTHID
+)
+,
+-- pre-eclampsia with chronic hyptertension
+icd_10_sipe AS
+(
+SELECT
+  a.BIRTHID,
+  MAX(CASE WHEN b.DX_DATE IS NOT NULL THEN 1 ELSE 0 END) AS 'sipe'
+FROM demographic_base a
+LEFT JOIN AMMI.dbo.DIAGNOSIS b ON a.MOTHERID = b.PATID
+ AND b.DX LIKE 'O11%'
+ AND b.DX_DATE BETWEEN DATEADD(WEEK, 20, a.preg_start_date) AND DATEADD(DAY, 90, a.preg_end_date)
+GROUP BY a.BIRTHID
+)
+,
+-- Proteinuria 
+lab_proteinuria AS
+(
+SELECT
+  a.BIRTHID,
+  MAX(CASE WHEN b.LAB_ORDER_DATE IS NOT NULL THEN 1 ELSE 0 END) AS 'lab_proteinuria'
+FROM demographic_base a
+LEFT JOIN AMMI.dbo.LAB_RESULT_CM b ON a.MOTHERID = b.PATID
+ AND b.LAB_ORDER_DATE BETWEEN DATEADD(WEEK, 20, a.preg_start_date) AND DATEADD(DAY, 90, a.preg_end_date)
+ AND (
+      (LAB_LOINC = '2889-4' AND -- protein in 24h urine > 300mg
+      ((SUBSTRING(LOWER(RESULT_UNIT),1,2)='mg' AND RESULT_NUM >= 300) OR 
+	   (SUBSTRING(LOWER(RESULT_UNIT),1,1)='g' AND RESULT_NUM >= 0.3))) 
+   OR (LAB_LOINC = '2890-2' AND -- urine protein/creatine (UPC) > 0.3
+      ((SUBSTRING(LOWER(RESULT_UNIT),1,2)='mg' AND RESULT_NUM >= 300) OR
+	   (SUBSTRING(LOWER(RESULT_UNIT),1,2)='mg' AND NORM_RANGE_HIGH='<=0.15' AND RESULT_NUM >= 0.3) OR
+	   (SUBSTRING(LOWER(RESULT_UNIT),1,1)='g' AND RESULT_NUM >= 0.3)))
+)
+GROUP BY a.BIRTHID
+)
+,
+-- other lab
+lab_others AS
+(
+SELECT
+  a.BIRTHID,
+  MAX(CASE WHEN b.LAB_ORDER_DATE IS NOT NULL THEN 1 ELSE 0 END) AS 'lab_others'
+FROM demographic_base a
+LEFT JOIN AMMI.dbo.LAB_RESULT_CM b ON a.MOTHERID = b.PATID
+ AND b.LAB_ORDER_DATE BETWEEN DATEADD(WEEK, 20, a.preg_start_date) AND DATEADD(DAY, 90, a.preg_end_date)
+ AND (
+      (LAB_LOINC = '1920-8' AND RESULT_NUM >= 66) -- AST >= 66
+   OR (LAB_LOINC = '1742-6' AND RESULT_NUM >= 72) -- ALT >= 72
+   OR (LAB_LOINC = '2160-0' AND LOWER(RESULT_UNIT) = 'mg/dl' AND RESULT_NUM > 1.1 AND RESULT_NUM < 10) -- ct >1.1, there're too many lab with the same loinc code; there's also unit mL/min not sure what to do
+   OR (LAB_LOINC = '777-3' AND SUBSTRING(RESULT_UNIT,1,2) = '10' AND RESULT_NUM < 100) -- plt < 100
+)
+GROUP BY a.BIRTHID
+)
+,
+-- finally the decision table
+decision_table AS
+(
+SELECT
+  a.BIRTHID,
+  a.pre_sf_or_ecl,
+  b.mag_infuse,
+  c.chtn_any,
+  d.bp_cat_after_20wk,
+  e.sipe,
+  f.lab_proteinuria,
+  g.lab_others
+FROM icd_10_pre_sf a
+LEFT JOIN mag_infuse b ON a.BIRTHID = b.BIRTHID
+LEFT JOIN chtn c ON a.BIRTHID = c.BIRTHID
+LEFT JOIN bp_cat d ON a.BIRTHID = d.BIRTHID
+LEFT JOIN icd_10_sipe e ON a.BIRTHID = e.BIRTHID
+LEFT JOIN lab_proteinuria f ON a.BIRTHID = f.BIRTHID
+LEFT JOIN lab_others g ON a.BIRTHID = g.BIRTHID
+)
+
+SELECT 
+  *,
+  CASE WHEN (chtn_any = 0 AND (pre_sf_or_ecl = 1 OR bp_cat_after_20wk = 2 OR mag_infuse = 1 OR sipe = 1)) THEN 1  -- i think sipe can be 1 here
+       WHEN (chtn_any = 0 AND bp_cat_after_20wk = 1 AND lab_others = 1) THEN 1 -- seems protienuria alone is not considered severe??
+	   WHEN (chtn_any = 1 AND (pre_sf_or_ecl = 1 OR sipe = 1 OR mag_infuse = 1)) THEN 1
+	   WHEN (chtn_any = 1 AND bp_cat_after_20wk = 2 AND lab_proteinuria = 1) THEN 1 -- this doesn't feel right to me, should include lab_others = 1??
+	   WHEN (chtn_any = 1 AND bp_cat_after_20wk = 1 AND lab_others = 1) THEN 1
+	   ELSE 0 END AS 'preeclampsia_label'
+FROM decision_table
+ORDER BY BIRTHID;
+'''
+
 get_rx_sql_string = demographic_base_sql_string + '''
 SELECT
   a.BIRTHID,
@@ -246,4 +453,5 @@ SELECT
 FROM demographic_base a LEFT JOIN AMMI.dbo.PRESCRIBING b ON a.MOTHERID = b.PATID
  AND b.RX_ORDER_DATE BETWEEN a.preg_start_date AND a.cutoff_time
 GROUP BY a.BIRTHID
+ORDER BY a.BIRTHID
 '''
