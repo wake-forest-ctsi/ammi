4bd32dcc28937bd8666301eb403794faf9f37d96
Michael Horvath <mhorvath@wakehealth.edu>
2024-11-13 12:53:34 -0500

    Add vitals and prescribing Re-worked base filter dates view

--- datamart/models/03_staging/pcornet/stg_pcornet__demographic.sql
+++ datamart/models/03_staging/pcornet/stg_pcornet__demographic.sql
@@ -1,33 +1,20 @@
+select
+    patid,
+    birth_date,
+    birth_time,
+    sex,
+    sexual_orientation,
+    gender_identity,
+    hispanic,
+    biobank_flag,
+    race,
+    pat_pref_language_spoken,
+    raw_sex,
+    raw_hispanic,
+    raw_race,
+    raw_sexual_orientation,
+    raw_gender_identity,
+    raw_pat_pref_language_spoken
 
-with source as (
-
-    select * from {{ source('pcornet', 'demographic') }}
-
-),
-
-renamed as (
-
-    select
-        patid,
-        birth_date,
-        birth_time,
-        sex,
-        sexual_orientation,
-        gender_identity,
-        hispanic,
-        biobank_flag,
-        race,
-        pat_pref_language_spoken,
-        raw_sex,
-        raw_hispanic,
-        raw_race,
-        raw_sexual_orientation,
-        raw_gender_identity,
-        raw_pat_pref_language_spoken
-
-    from source
-
-)
-
-select * from renamed
+from {{ source('pcornet', 'demographic') }}
 
--- datamart/models/06_reports/ammi/gest.sql
+++
@@ -1,4 +0,0 @@
-select
-    *
-from
-    {{ ref('int_obs_clin__max_gestational_ages')}}
