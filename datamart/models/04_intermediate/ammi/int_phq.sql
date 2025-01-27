dcdf2ad288a7ab2c82de8d9c0c1940a88ec59ad0
Zhitu Ma <zhma@wakehealth.edu>
2025-01-27 17:00:12 -0500

    added phq feature and update the report on depression without dx code

---
+++ datamart/models/04_intermediate/ammi/int_phq.sql
@@ -0,0 +1,78 @@
+-- depends_on: {{ ref('daterange') }}
+
+{% set features = ( '13011', '21012976', '21012977',
+      '21012954', '21012949', '21012951', '21012960', '21012948', 
+      '21012956', '21012953', '21012958', '21012955', '21012950') %}
+
+{% set date_range_list = get_date_range('int_obs_clin_features') %}
+
+with cohort as (
+    select
+        *
+    from {{ ref('int_cohort') }}
+),
+
+phq as (
+    select
+        patid,
+        obsclin_start_date,
+        obsclin_code,
+        case 
+            when obsclin_code in ('21012954', '21012949', '21012951', '21012948', '21012956', 
+                                  '21012953', '21012958', '21012955', '21012950') then
+                case when obsclin_result_text = 'Not at all' then 0
+                     when obsclin_result_text = 'Several days' then 1
+                     when obsclin_result_text = 'More than half the days' then 2
+                     when obsclin_result_text = 'Nearly every day' then 3
+                     when obsclin_result_text = 'No information' then NULL
+                     else -99 end
+            when obsclin_code in ('21012960') then
+                case when obsclin_result_text = 'Not difficult at all' then 0
+                     when obsclin_result_text = 'Somewhat difficult' then 1
+                     when obsclin_result_text = 'Very difficult' then 2
+                     when obsclin_result_text = 'Extremely difficult' then 3
+                     when obsclin_result_text = 'No information' then NULL
+                     else -99 end
+            when obsclin_code in ('21012977', '21012976') then
+                case when obsclin_result_text = 'No' then 0
+                     when obsclin_result_text = 'Yes' then 1
+                     when obsclin_result_text = 'No information' then NULL
+                     else -99 end
+            when obsclin_code in ('13011') then
+                case when obsclin_result_text = 'Patient lacks the functional capacity to participa' then 3
+                     when obsclin_result_text = 'Patient Refusal' then 2
+                     when obsclin_result_text = 'PHQ9 Preferred' then 1
+                     else 0 end
+            else
+                -99
+        end as phq_value
+    from {{ ref('stg_pcornet__obs_clin') }}
+    where obsclin_code is not null
+      and obsclin_code in {{ features }} 
+),
+
+phq_stats as (
+    select
+        cohort.birthid,
+        obsclin_code,
+        count(phq_value) as value_counts,
+        min(phq_value) as value_min,
+        max(phq_value) as value_max,
+        avg(phq_value) as value_mean
+    from cohort
+    left join phq on cohort.mother_patid = phq.patid
+     and obsclin_start_date between {{ date_range_list[0] }} and {{ date_range_list[1] }}
+    group by cohort.birthid, obsclin_code
+),
+
+renamed as (
+    select
+        birthid,
+        {% for feature in features %}
+            sum(case when obsclin_code = '{{ feature }}' then value_max else null end) as 'phq_{{ feature }}_max' {% if not loop.last %},{% endif %}
+        {% endfor %}
+    from phq_stats
+    group by birthid
+)
+
+select * from renamed
---
+++ datamart/models/06_reports/ammi/depression/rpt_depression_nodx.sql
@@ -0,0 +1,46 @@
+select
+    a.birthid,
+    b.mother_age,
+    c.mother_is_black,
+    c.mother_is_hispanic,
+    c.mother_is_white,
+    d.gest_age_in_days,
+    d.gest_age_is_null,
+    {{ dbt_utils.star(from=ref('int_insurance_pivoted'), except=['birthid']) }},
+    {{ dbt_utils.star(from=ref('int_visit_pattern'), except=['birthid']) }},
+    g.phq_21012976_max,
+    g.phq_21012977_max,
+    {{ dbt_utils.star(from=ref('int_delivery_mode_pivoted'), except=['birthid']) }},
+    j.parity,
+    k.parity_1_recovered,
+    k.parity_2_recovered,
+    l.original_bmi_mean,
+    l.computed_bmi_mean,
+    m.smoking,
+    m.tobacco,
+    n.morbid,
+    n.obese,
+    n.overweight,
+    n.O99_obese,
+    {{ dbt_utils.star(from=ref('int_censustract_features'), 
+                      except=['birthid', 'addressid', 'zipcode', 'tractfips', 'longitude', 'latitude']) }},
+    p.mother_height,
+    (case when q.earliest_ppd_diagnosis_date is not null then 1 else 0 end) as ppd_label,
+    r.edinburgh_max
+from {{ ref('int_cohort') }} a
+left join {{ ref('int_mother_age_at_birth') }} b on a.birthid = b.birthid
+left join {{ ref('int_race') }} c on a.mother_patid = c.mother_patid
+left join {{ ref('int_gestational_age') }} d on a.birthid = d.birthid
+left join {{ ref('int_insurance_pivoted') }} e on a.birthid = e.birthid
+left join {{ ref('int_visit_pattern') }} f on a.birthid = f.birthid
+left join {{ ref('int_phq') }} g on a.birthid = g.birthid
+left join {{ ref('int_delivery_mode_pivoted') }} i on a.birthid = i.birthid
+left join {{ ref('int_parity') }} j on a.birthid = j.birthid
+left join {{ ref('int_other_parity') }} k on a.birthid = k.birthid
+left join {{ ref('int_vital_features') }} l on a.birthid = l.birthid
+left join {{ ref('int_smoking') }} m on a.birthid = m.birthid
+left join {{ ref('int_other_obese') }} n on a.birthid = n.birthid
+left join {{ ref('int_censustract_features') }} o on a.birthid = o.birthid
+left join {{ ref('int_mother_height') }} p on a.birthid = p.birthid
+left join {{ ref('int_postpartum_depression') }} q on a.birthid = q.birthid
+left join {{ ref('int_edinburgh') }} r on a.birthid = r.birthid
