7955167ddb27f94ef99574989607f15f558184d9
Zhitu Ma <zhma@wakehealth.edu>
2025-04-22 11:42:07 -0400

    added all preprocessing steps in to preprocess.py; added test.ipynb

--- analysis/zhma_ml_analysis/postpardum_depression_analysis/preprocess.py
+++ analysis/zhma_ml_analysis/postpardum_depression_analysis/preprocess.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-def preprocess (dat, split='train'):
+def preprocess (dat):
     # parity has a lot of nan, fill with 1 and mark the nan entry
     dat['parity'] = pd.to_numeric(dat['parity'], errors='coerce')  # the type may sometimes be Object
     dat['parity_isna'] = np.where(dat['parity'].isna(), 1, 0)
@@ -49,15 +49,29 @@ def preprocess (dat, split='train'):
     dat.dropna(subset=subset, inplace=True)
 
     # filter out patients without prenatal care visit or postpartum visit
-    # only for train split
-    if (split == 'train'):
-        condition_1 = (dat['counts_of_visits_prenatal_care'] == 0)
-        condition_2 = (dat['counts_of_visits_3m_after_delivery'] == 0)
-        tmp = dat[condition_1 | condition_2]
-        print(f"removing patients without prenatal and postpartum visit {len(tmp)}")
-        dat.drop(tmp.index, inplace=True)
+    condition_1 = (dat['counts_of_visits_prenatal_care'] == 0)
+    condition_2 = (dat['counts_of_visits_3m_after_delivery'] == 0)
+    tmp = dat[condition_1 | condition_2]
+    print(f"removing patients without prenatal and postpartum visit {len(tmp)}")
+    dat.drop(tmp.index, inplace=True)
     
-    # no matter whether it's in train or test, always drop the columns after delivery
+    # drop the columns after delivery
     print(f"dropping columns: counts_of_visits_3m_after_delivery, counts_of_visits_6m_after_delivery")
     dat.drop(columns=['counts_of_visits_3m_after_delivery', 'counts_of_visits_6m_after_delivery'], 
              inplace=True)
+    
+    # drop the patients without phq or edinburgh screening
+    condition_1 = dat['edinburgh_max'].isna()
+    condition_2 = dat['phq9_total_max'].isna()
+    tmp = dat[condition_1 & condition_2]
+    print(f"removing patients without screening using edinburgh or phq9 {len(tmp)}")
+    dat.drop(tmp.index, inplace=True)
+
+    # NOTE: may want to adjust these to reflect the screening tools used at your institue
+    # create label using F53 or edinburgh_max or phq9
+    # drop other definitions of ppd that are not used at WFU
+    dat['label'] = np.where( (dat['edinburgh_max'] >= 10) | 
+                             (dat['phq9_total_max'] >= 10) | 
+                             (dat['F53_label'] == 1), 1, 0)
+    print(f"removing columns: 'F53_label','edinburgh_max','phq9_total_max', 'PPD_delete_label'")
+    dat.drop(columns=['F53_label','edinburgh_max','phq9_total_max', 'PPD_delete_label'], inplace=True)
