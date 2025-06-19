986c3315d2f417d529d3db615472ad25a9207c82
Zhitu Ma <zhma@wakehealth.edu>
2025-06-19 13:32:28 -0400

    update bp_cat_macro to be more efficient

--- datamart/macros/bp_cat_macro.sql
+++ datamart/macros/bp_cat_macro.sql
@@ -17,37 +17,45 @@ vital as (
     where systolic is not null and diastolic is not null
 ),
 
--- select birthid with bp_cat >= 1
-vital_4h_bp_cat_1 as(
+vital_within_time as (
     select
-        cohort.birthid
+        cohort.birthid,
+        vital.measure_date,
+        vital.bp_cat
     from cohort
     left join vital on cohort.mother_patid = vital.patid
      and vital.measure_date between {{ date1 }} and {{ date2 }}
-     and bp_cat >= 1
-    group by cohort.birthid
+),
+
+-- select birthid with bp_cat >= 1
+vital_4h_bp_cat_1 as(
+    select
+        birthid
+    from vital_within_time
+    where bp_cat >= 1
+    group by birthid
     having datediff(hour, min(measure_date), max(measure_date)) >= 4
 ),
 
 -- select birthid with bp_cat == 2
 vital_4h_bp_cat_2 as (
     select
-        cohort.birthid
-    from cohort
-    left join vital on cohort.mother_patid = vital.patid
-     and vital.measure_date between {{ date1 }} and {{ date2 }}
-     and bp_cat = 2
-    group by cohort.birthid
+        birthid
+    from vital_within_time 
+    where bp_cat = 2
+    group by birthid
     having datediff(hour, min(measure_date), max(measure_date)) >= 4
 ),
 
 bp_cat as (
     select
-        birthid,
-        case when birthid in (select birthid from vital_4h_bp_cat_2) then 2
-             when birthid in (select birthid from vital_4h_bp_cat_1) then 1
+        cohort.birthid,
+        case when v2.birthid is not null then 2
+             when v1.birthid is not null then 1
              else 0 end as bp_cat
     from cohort
+    left join vital_4h_bp_cat_1 v1 on cohort.birthid = v1.birthid
+    left join vital_4h_bp_cat_2 v2 on cohort.birthid = v2.birthid
 ),
 
 
@@ -57,19 +65,17 @@ no_bp as (
         cohort.birthid,
         min(case when bp_cat is null then 1 else 0 end) as nobp
     from cohort
-    left join vital on cohort.mother_patid = vital.patid
-     and vital.measure_date between {{ date1 }} and {{ date2 }}
+    left join vital_within_time on cohort.birthid = vital_within_time.birthid
     group by cohort.birthid
 ),
 
 renamed as (
     select
-        cohort.birthid,
+        bp_cat.birthid,
         bp_cat,
         nobp
-    from cohort
-    left join bp_cat on cohort.birthid = bp_cat.birthid
-    left join no_bp on cohort.birthid = no_bp.birthid
+    from bp_cat
+    left join no_bp on bp_cat.birthid = no_bp.birthid
 )
 
 select * from renamed
